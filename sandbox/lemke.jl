7eeffc0d8b2ef4b07ce8c90a4bcd73c72c0a6ff4
Andras Kiss <kiss.andras@gmail.com>
2014-01-04 22:54:14 +0000

    achieved fast, clean and bug-free pivot. sparsity destruction is still an issue in big problems!

--- sandbox/lemke.jl
+++ sandbox/lemke.jl
@@ -2,24 +2,24 @@
 # This is a learning exercise, so don't expect nice code :)
 
 
-function lemke(M::AbstractArray{Float64,2}, q::Array{Float64,1}; maxiter::Integer=1000)
+function lemke(M :: AbstractArray{Float64,2}, q :: Array{Float64,1}; maxiter :: Integer = 1000)
     const n = length(q)
     d = sparse(1.0 * (q.<0))
     if sum(d) == 0.0
         println("Trivial solution exists.")
         return zeros((n, 1))
     end
-    A::AbstractArray{Float64,2} = [copy(M) spzeros(n) d]
-    b::Array{Float64,1} = copy(q)
-    basic::Array{Integer,1} = [n+1:2n]
-    enter::Integer = 2n+1
-    pivot::Integer = indmin(b)
-    leave::Integer = basic[pivot]
-    @time pivot!(A, b, basic, pivot, enter)
+    A :: AbstractArray{Float64,2} = [copy(M) spzeros(n) d]
+    b :: Array{Float64,1} = copy(q)
+    basic :: Array{Integer,1} = [n+1:2n]
+    enter :: Integer = 2n+1
+    pivrow :: Integer = indmin(b)
+    leave :: Integer = basic[pivrow]
+    pivot!(A, b, basic, pivrow, enter)
     for iter in 1:maxiter
         if leave==2n+1
             println("I've found a solution!")
-            solution::Array{Float64,1} = zeros(n)
+            solution :: Array{Float64,1} = zeros(n)
             for i in 1:n
                 if basic[i] <= n
                     solution[basic[i]] = b[i]
@@ -32,16 +32,16 @@ function lemke(M::AbstractArray{Float64,2}, q::Array{Float64,1}; maxiter::Intege
             println("Algorithm ended up on a secondary ray")
             return
         end
-        pivot = 0
+        pivrow = 0
         v = Inf
         for i in 1:n
             if A[i, enter] < 0 && -b[i] / A[i, enter] < v
-                pivot = i
+                pivrow = i
                 v = -b[i] / A[i, enter]
             end
         end
-        leave = basic[pivot]
-        @time pivot!(A, b, basic, pivot, enter)
+        leave = basic[pivrow]
+        @time pivot!(A, b, basic, pivrow, enter)
     end
 
     println("Maximum number of iterations reached!")
@@ -59,35 +59,27 @@ end
 
 
 
-function pivot!(A :: AbstractArray{Float64,2}, b :: Array{Float64,1}, basic :: Array{Integer,1}, pivot :: Integer, enter :: Integer)
+function pivot!(A :: AbstractArray{Float64,2}, b :: Array{Float64,1}, basic :: Array{Integer,1}, pivrow :: Integer, enter :: Integer)
+    const zerotol :: Float64 = 1e-3
     const n :: Int32 = length(b)
-    const d :: Float64 = -A[pivot, enter]
-    const v :: Float64 = b[pivot] / d
-    A[pivot, basic[pivot]] = -1.0
-    A[pivot, enter] = 0.0
-    I, J, V = findnz(A)
-    const allpivcols :: Array{Int32,1} = [1:2n+1][bitunpack(slice(A, pivot, :) .!= 0.0)]
-    const allpivrows :: Array{Int32,1} = [1:n][bitunpack(slice(A, :, enter) .!= 0.0)]
-    const pivotrow::Array{Bool,1} = I.==pivot
-    const entercol::Array{Bool,1} = J.==enter
-    const numpivrows :: Integer = length(allpivrows)
-    const numpivcols :: Integer = length(allpivcols)
-    V[pivotrow] /= d    
-    I2 :: Array{Int32,1} = zeros(numpivrows * numpivcols)
-    J2 :: Array{Int32,1} = zeros(numpivrows * numpivcols)
-    V2 :: Array{Float64,1} = zeros(numpivrows * numpivcols)
-    for j in 1:numpivcols, i in 1:numpivrows
-        ix = (j - 1) * numpivrows + i
-        I2[ix] = allpivrows[i]
-        J2[ix] = allpivcols[j]
-        V2[ix] = A[i, enter] * A[pivot, j]
-    end
-    for i in allpivrows
-        b[i] += A[i, enter] * v
-    end
-    V[entercol] = 0.0
-    b[pivot] = v
-    basic[pivot] = enter
-    A = sparse([I; I2], [J; J2], [V; V2])
+    const d :: Float64 = -A[pivrow, enter]
+    const v :: Float64 = b[pivrow] / d
+    A[pivrow, basic[pivrow]] = -1.0
+    A[pivrow, enter] = 0.0
+    A[pivrow, :] /= d
+    const pivrowixs :: Array{Int32,1} = [1:n][bitunpack(slice(A, :, enter) .!= 0.0)]
+    const pivcolixs :: Array{Int32,1} = [1:2n+1][bitunpack(slice(A, pivrow, :) .!= 0.0)]
+    
+    Ap :: Array{Float64,2} = full(A[pivrowixs, pivcolixs] + A[pivrowixs, enter] * A[pivrow, pivcolixs])
+    setindex!(Ap, 0.0, -zerotol .< Ap .< zerotol)
+    setindex!(A, sparse(Ap), pivrowixs, pivcolixs)
+
+    setindex!(b, b[pivrowixs] + A[pivrowixs, enter] * v, pivrowixs)
+
+    setindex!(A, 0.0, pivrowixs, [enter])
+
+    b[pivrow] = v
+
+    basic[pivrow] = enter
 end
 
